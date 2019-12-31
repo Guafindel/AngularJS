d1888998127e7313516094f2cad34b8dd059f0a6
Guafindel <50958613+Guafindel@users.noreply.github.com>
2019-12-31 22:28:38 +0900

    12-31 / 22:28
    todo 리스트 만들기 repeat관련 배열로 출력하기.
    ngFor문을 알아야할 필요성 절감

--- AngularBasic/script.js
+++ AngularBasic/script.js
@@ -1,13 +1,43 @@
-(function () {
+(function() {
     var app = angular.module('todo', []);
 
     app.controller('TodoCtrl', ['$scope', function($scope) {
-        $scope.todo = {
-            title: '요가수련',
-            completed: false,
-            createdAt: Date.now()
-        }
+        $scope.todos = [{
+                title: '요가 수행',
+                completed: false,
+                createdAt: Date.now()
+            },
+            {
+                title: '앵귤러 학습',
+                completed: false,
+                createdAt: Date.now()
+            },
+            {
+                title: '게임 하기',
+                completed: true,
+                createdAt: Date.now()
+            }
+        ]
 
+    }]);
+
+    app.controller('hero', ['$scope', function($scope) {
+        $scope.heroes = [{
+                title: '마블',
+                name: '위도우 메이커',
+                level: 3
+            },
+            {
+                title: '오버워치',
+                name: '파라',
+                level: 4
+            },
+            {
+                title: '배트맨',
+                name: '슈퍼맨',
+                level: 99
+            }
+        ]
     }]);
 
 })();

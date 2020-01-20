4f22ca543ce0f47ca791cec801821baf6d5e74b5
Guafindel <50958613+Guafindel@users.noreply.github.com>
2020-01-20 18:46:48 +0900

    1231

--- AngularTest/index.js
+++ AngularTest/index.js
@@ -13,4 +13,44 @@ test.controller('TestCtrl', function ($scope) {
         day: '2020-01-02',
         time: date.setHours(09, 30)
     }];
+
+    $scope.timeFormatter = function(time) {
+
+        var ampm = new Date(time).toLocaleTimeString('en-US', {
+            hour : 'numeric',
+            hour12 : true,
+            minute : 'numeric'
+        });
+        console.log(ampm);
+
+        return ampm;
+     
+    }
+
+    $scope.getTime = function(time) {
+        var getDate = new Date(time);
+
+        console.log(getDate);
+        return getDate;
+
+    }
+
+    $scope.cutStringDate = function(time) {
+        var getDate = time.substring(0, 10);
+        console.log(getDate);
+        console.log(typeof getDate);
+        return getDate;
+    }
+
+   $scope.checkYear = function() {
+       var nowDate;
+       var nowDate2;
+
+       nowDate = date.getFullYear;
+       nowDate2 = date.getMonth;
+       console.log(nowDate);
+       console.log(nowDate2);
+   }
+
+    
 });
